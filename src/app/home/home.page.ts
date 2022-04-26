9c3c70b3701a564e41435fac656f076de12f162b
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-04-25 22:07:07 -0600

    Manipulating ion-datetime component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { format, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-home',
@@ -9,25 +10,26 @@ export class HomePage {
 
   modes = ['date', 'date-time', 'month', 'month-year', 'time', 'time-date', 'year'];
   selectedMode = 'date';
+  showPicker = false;
+  dateValue = format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z';
+  formattedString = '';
 
-  constructor() { }
-
-  ionViewDidEnter() {
-    console.log(this.selectedMode);
-
-  }
-  onChange(event) {
-    console.log(event);
-
+  constructor() {
+    this.setToday();
   }
 
-  segmentChanged(event) {
-    console.log(event);
-
+  setToday() {
+    this.formattedString = format(parseISO(format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z'), 'HH:mm, MMM d, yyyy');
   }
 
-  // presentationChange(){
-  //   this.selectedMode
-  // }
+  dateChanged(value) {
+    console.log('test');
+    console.log(this.dateValue);
+    console.log(value);
+
+    this.dateValue = value;
+    this.formattedString = format(parseISO(value), 'HH:mm, MMM d, yyyy');
+    this.showPicker = false;
+  }
 
 }
